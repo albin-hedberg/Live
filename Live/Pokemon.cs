6e9e280e0ad3362e7e1fbf0f1fb23b15fe6d551d
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-09-11 10:21:58 +0200

    Add project files.

---
+++ Live/Pokemon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveKodPokemon
+{
+    internal class Pokemon
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } } // Properties (get/set funktioner)
+
+        public string _type;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine(_type);
+            Console.WriteLine();
+        }
+
+        public Pokemon()
+        {
+            ;
+        }
+
+        public Pokemon(string name, string type)
+        {
+            _name = name;
+            _type = type;
+        }
+    }
+}
+
+// variabler med '_' = field/fält
+// ctor - konstruktor ac
+// static - körs på typen, inte instansen
+// 
---
+++ Live/Program.cs
@@ -0,0 +1,14 @@
+﻿using LiveKodPokemon;
+
+Pokemon pkm1 = new Pokemon("Charmander", "Fire");
+Pokemon pkm2 = new Pokemon("Bulbaqwerty", "Grass");
+
+Pokemon pkm3 = new Pokemon();
+pkm3.Name = "Pikapika";
+pkm3._type = "Electric";
+
+pkm1.PrintInfo();
+pkm2.PrintInfo();
+
+Console.WriteLine(pkm3.Name);
+Console.WriteLine(pkm3._type);
