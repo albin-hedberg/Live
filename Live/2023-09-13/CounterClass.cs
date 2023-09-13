d0591577c0173819cf026ad940dd3b156e125360
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-09-13 10:00:59 +0200

    Live kodning - Ons 13 Sep, 2023

---
+++ Live/2023-09-13/CounterClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live
+{
+    internal class CounterClass
+    {
+        public int Counter { get; private set; }
+
+        public CounterClass()
+        {
+            Counter = 10;
+        }
+
+        public void CountUp()
+        {
+            Counter++;
+        }
+
+        public void CountDown()
+        {
+            Counter--;
+        }
+
+        public override string ToString()
+        {
+            return $"Current count: {Counter}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            // Om object är av samm typ/referens? som...
+            if (obj is CounterClass counterClass)
+            {
+                if (counterClass.Counter == Counter)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Live/Program.cs
+++ Live/Program.cs
@@ -1,25 +1,48 @@
 ﻿using Live;
-// Live kodning - 
+// Live kodning - [date]
+// Ctrl+Space = intelisense
 //-------------------- Mån 11 Sep, 2023 --------------------
-Pokemon pkm1 = new Pokemon("Charmander", PokeTypes.Electric);
-Pokemon pkm2 = new Pokemon("Bulbaqwerty", (PokeTypes)3);
+//Pokemon pkm1 = new Pokemon("Charmander", PokeTypes.Electric);
+//Pokemon pkm2 = new Pokemon("Bulbaqwerty", (PokeTypes)3);
 
-//Pokemon pkm3 = new Pokemon();
-//pkm3.Name = "Pikapika";
-//pkm3.Type = "Electric";
+////Pokemon pkm3 = new Pokemon();
+////pkm3.Name = "Pikapika";
+////pkm3.Type = "Electric";
 
-pkm1.PrintInfo();
-pkm2.PrintInfo();
+//pkm1.PrintInfo();
+//pkm2.PrintInfo();
 
-pkm1.Attack(pkm2);
-//pkm2.Attack(pkm1);
-Console.WriteLine(pkm1.Name + " HP: " + pkm1.HealthPoints);
-Console.WriteLine(pkm2.Name + " HP: " + pkm2.HealthPoints);
-//Pokemon.AttackStatic();
-//Trainer.Greeting();
+//pkm1.Attack(pkm2);
+////pkm2.Attack(pkm1);
+//Console.WriteLine(pkm1.Name + " HP: " + pkm1.HealthPoints);
+//Console.WriteLine(pkm2.Name + " HP: " + pkm2.HealthPoints);
+////Pokemon.AttackStatic();
+////Trainer.Greeting();
 
-//pkm3.Type = "Tjohoo";
+////pkm3.Type = "Tjohoo";
 
-// Övningsuppgifter 1-18  (Programmering 1)
-// Övningsuppgifter 18-40 (Programmering 2)
+//// Övningsuppgifter 1-18  (Programmering 1)
+//// Övningsuppgifter 18-40 (Programmering 2)
+//----------------------------------------------------------
+
+//-------------------- Ons 13 Sep, 2023 --------------------
+CounterClass counterClass = new CounterClass();
+CounterClass counterClass2 = new CounterClass();
+
+counterClass2.CountUp();
+
+// Jämför referensen, 2 olika instanser = 2 olika referenser
+//if(counterClass == counterClass2)
+
+if(counterClass.Equals(counterClass2))
+{
+    Console.WriteLine("Dom är samma!");
+}
+else
+{
+    Console.WriteLine("Dom är inte samma!");
+}
+
+Console.WriteLine(counterClass.ToString());
+Console.WriteLine(counterClass2.ToString());
 //----------------------------------------------------------
